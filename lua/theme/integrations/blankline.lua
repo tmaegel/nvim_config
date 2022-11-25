332691d1a999dccbeefb3f5fa0962f218ff55bcd
Toni Mägel <tmaegel@posteo.de>
2022-11-25 15:27:03 +0100

    Disable indent-blankline context highlight

--- lua/theme/integrations/blankline.lua
+++ lua/theme/integrations/blankline.lua
@@ -3,6 +3,6 @@ local colors = require("theme").get_theme_tb "base_30"
 return {
   IndentBlanklineChar = { fg = colors.line },
   IndentBlanklineSpaceChar = { fg = colors.line },
-  IndentBlanklineContextChar = { fg = colors.grey },
+  IndentBlanklineContextChar = { fg = colors.line },
   IndentBlanklineContextStart = { bg = colors.one_bg2 },
 }
