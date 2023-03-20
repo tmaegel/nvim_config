d596fa4d4958441b73917d870afed394c3be7857
Toni Mägel <tmaegel@posteo.de>
2023-03-20 18:20:42 +0100

    Update theme color for whichkey

--- lua/theme/integrations/whichkey.lua
+++ lua/theme/integrations/whichkey.lua
@@ -6,4 +6,6 @@ return {
   WhichKeyDesc = { fg = colors.red },
   WhichKeyGroup = { fg = colors.green },
   WhichKeyValue = { fg = colors.green },
+  WhichKeyFloat = { bg = colors.black },
+  WhichKeyBorder = { fg = colors.grey, bg = colors.black },
 }
