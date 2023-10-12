622717da2e382dca5aa2e8c03b51317a3f96a5d4
Toni Mägel <tmaegel@posteo.de>
2023-10-12 15:55:59 +0200

    Update plugins

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,6 +1,7 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp", -- Snippets source for nvim-cmp
       "hrsh7th/cmp-nvim-lsp-signature-help", -- Source for displaying function signatures
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
   -- Until https://github.com/nvim-treesitter/nvim-treesitter/issues/4945 is solved.
   commit = "33eb472b459f1d2bf49e16154726743ab3ca1c6d",
   config = function()
@@ -30,6 +32,15 @@ return {
       },
       indent = {
         enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
       },
     }
   end,
