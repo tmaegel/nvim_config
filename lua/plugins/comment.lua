75e6b0b7bbb6739c305efa80891e1da67af3798a
Toni Mägel <tmaegel@posteo.de>
2024-01-06 13:49:11 +0100

    chore(Comment.nvim): Stick to v0.8.0

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,7 @@
+-- https://github.com/numToStr/Comment.nvim
 return {
   "numToStr/Comment.nvim",
+  tag = "v0.8.0",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     require("Comment").setup()
