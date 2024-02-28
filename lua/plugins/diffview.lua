77d876a746fe4e2fdf53454c19a6aaed2a8366ea
Toni Mägel <tmaegel@posteo.de>
2024-02-28 10:07:03 +0100

    feat(diffview): Add diffview plugin

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,59 @@
+-- https://github.com/sindrets/diffview.nvim
+return {
+  "sindrets/diffview.nvim",
+  config = function()
+    require("diffview").setup {
+      show_help_hints = false,
+      use_icons = true,
+      icons = { -- Only applies when use_icons is true.
+        folder_closed = "󰉖",
+        folder_open = "󰷏",
+      },
+      signs = {
+        fold_closed = "",
+        fold_open = "",
+        done = "✓",
+      },
+      view = {
+        -- Configure the layout and behavior of different types of views.
+        -- Available layouts:
+        --  'diff1_plain'
+        --    |'diff2_horizontal'
+        --    |'diff2_vertical'
+        --    |'diff3_horizontal'
+        --    |'diff3_vertical'
+        --    |'diff3_mixed'
+        --    |'diff4_mixed'
+        -- For more info, see ':h diffview-config-view.x.layout'.
+        default = {
+          -- Config for changed files, and staged files in diff views.
+          layout = "diff2_horizontal",
+          winbar_info = false, -- See ':h diffview-config-view.x.winbar_info'
+        },
+        merge_tool = {
+          -- Config for conflicted files in diff views during a merge or rebase.
+          layout = "diff3_horizontal",
+          disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
+          winbar_info = true, -- See ':h diffview-config-view.x.winbar_info'
+        },
+        file_history = {
+          -- Config for changed files in file history views.
+          layout = "diff2_horizontal",
+          winbar_info = false, -- See ':h diffview-config-view.x.winbar_info'
+        },
+      },
+      file_panel = {
+        listing_style = "list", -- One of 'list' or 'tree'
+        tree_options = { -- Only applies when listing_style is 'tree'
+          flatten_dirs = true, -- Flatten dirs that only contain one single dir
+          folder_statuses = "only_folded", -- One of 'never', 'only_folded' or 'always'.
+        },
+        win_config = { -- See ':h diffview-config-win_config'
+          position = "bottom",
+          height = 20,
+          win_opts = {},
+        },
+      },
+    }
+  end,
+}
