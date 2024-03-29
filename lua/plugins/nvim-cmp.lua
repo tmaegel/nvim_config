39816177353d98a5853572b3c0bcd13e1e9f8730
Toni Mägel <tmaegel@posteo.de>
2024-03-29 10:53:51 +0100

    chore(lazy.nvim): Update

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -103,7 +103,7 @@ return {
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<ESC>"] = cmp.mapping.abort(),
+          ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm {
             -- behavior = cmp.ConfirmBehavior.Replace,
             select = true,
