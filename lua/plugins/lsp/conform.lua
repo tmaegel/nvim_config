3db6f00fe5a73d381f6f5e33ae75001519a1cff2
Toni Mägel <tmaegel@posteo.de>
2024-05-05 12:03:41 +0200

    plugins: Add formatter plugin: conform

---
+++ lua/plugins/lsp/conform.lua
@@ -0,0 +1,42 @@
+-- https://github.com/stevearc/conform.nvim
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      dart = { "dart_format" },
+      go = { "golines", "gofmt" },
+      sh = { "shfmt" },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+      html = { "prettier" },
+      css = { "prettier" },
+      markdown = { "prettier" },
+      json = { "jq" },
+      terraform = { "terraform_fmt" },
+      -- Use the "*" filetype to run formatters on all filetypes.
+      ["*"] = {},
+      -- Use the "_" filetype to run formatters on filetypes that don't
+      -- have other formatters configured.
+      ["_"] = { "trim_whitespace" },
+    },
+    format_on_save = {
+      timeout_ms = 3000,
+      lsp_fallback = true,
+    },
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2", "-s", "-bn", "-ci", "-sr" },
+      },
+      prettier = {
+        prepend_args = { "--print-width", "120" },
+      },
+      jq = {
+        prepend_args = { "--indent", "4" },
+      },
+    },
+  },
+}
--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -11,8 +11,6 @@ return {
       -- PYTHON
       b.diagnostics.flake8,
       b.diagnostics.mypy,
-      b.formatting.isort,
-      b.formatting.black,
 
       -- SHELL / BASH
       b.code_actions.shellcheck,
@@ -23,43 +21,6 @@ return {
           "all",
         },
       },
-      b.formatting.shfmt.with {
-        extra_args = {
-          "-i",
-          "2",
-          "-s",
-          "-bn",
-          "-ci",
-          "-sr",
-        },
-      },
-
-      -- GOLANG
-      -- b.formatting.gofmt,
-      -- By default, the tool will use goimports as the base formatter (if found),
-      -- otherwise it will revert to gofmt.
-      b.formatting.golines,
-
-      -- JAVASCRIPT, CSS & HTML
-      b.formatting.prettier.with {
-        filetypes = {
-          "javascript",
-          "javascriptreact",
-          "typescript",
-          "typescriptreact",
-          "html",
-          "css",
-          "markdown",
-        },
-        extra_args = {
-          "--print-width",
-          "120",
-        },
-      },
-      b.formatting.djhtml,
-
-      -- LUA
-      b.formatting.stylua,
 
       -- YAML
       b.diagnostics.yamllint.with {
@@ -69,17 +30,6 @@ return {
         },
       },
 
-      -- JSON
-      b.formatting.json_tool.with {
-        extra_args = {
-          "--indent",
-          "4",
-        },
-      },
-
-      -- DART
-      b.formatting.dart_format,
-
       -- MARKDOWN
       b.diagnostics.markdownlint.with {
         extra_args = {
@@ -87,43 +37,11 @@ return {
           "MD013",
         },
       },
-
-      -- TERRAFORM
-      b.formatting.terraform_fmt,
-
-      -- OTHER
-      b.formatting.trim_whitespace,
-      b.formatting.trim_newlines,
     }
 
     null_ls.setup {
       -- Use :NullLsLog to open your debug log in the current Neovim
       debug = false,
-      -- Format on save
-      -- you can reuse a shared lspconfig on_attach callback here
-      on_attach = function(client, bufnr)
-        if client.supports_method "textDocument/formatting" then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-              vim.lsp.buf.format {
-                bufnr = bufnr,
-                -- On Neovim v0.8+, when calling vim.lsp.buf.format you may want to filter
-                -- the available formatters so that only null-ls receives the formatting request.
-                -- Otherwise, when calling vim.lsp.buf.format, other formatters from other clients
-                -- attached to the buffer may attempt to perform a format.
-                filter = function(client)
-                  return client.name == "null-ls"
-                end,
-              }
-              -- vim.lsp.buf.formatting_sync()
-            end,
-          })
-        end
-      end,
       sources = sources,
     }
   end,
