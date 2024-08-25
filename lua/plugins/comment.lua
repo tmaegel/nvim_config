22181ac463dfa30738f78c0a520f4b9da71dd4db
Toni Mägel <tmaegel@posteo.de>
2024-08-25 15:29:51 +0200

    Follow main branch for conform.nvim plugin

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,6 @@
 -- https://github.com/numToStr/Comment.nvim
 return {
   "numToStr/Comment.nvim",
-  tag = "v0.8.0",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     require("Comment").setup()
