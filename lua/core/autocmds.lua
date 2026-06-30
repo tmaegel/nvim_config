197791cabe43f5f18aa01419c59f1e90748fcbae
Toni Mägel <tmaegel@posteo.de>
2026-06-30 22:06:46 +0200

    feat: Remove none-ls, add nvim-lint

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -181,3 +181,11 @@ autocmd({ "BufWritePre" }, {
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
+
+autocmd({ "BufWritePost" }, {
+  callback = function()
+    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+    -- for the current filetype
+    require("lint").try_lint()
+  end,
+})
