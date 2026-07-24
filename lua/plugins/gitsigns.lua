05c26b2d5c8e5aaff61c56acf91ec9b2f2ca6980
Toni Mägel <tmaegel@posteo.de>
2026-07-24 15:33:41 +0200

    feat: Update gitsigns and trouble

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,6 @@
 -- https://github.com/lewis6991/gitsigns.nvim
 return {
   "lewis6991/gitsigns.nvim",
-  tag = "v0.9.0",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     require("gitsigns").setup {
@@ -13,6 +12,15 @@ return {
         changedelete = { text = "┃" },
         untracked = { text = "┃" },
       },
+      signs_staged = {
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "┃" },
+        topdelete = { text = "┃" },
+        changedelete = { text = "┃" },
+        untracked = { text = "┃" },
+      },
+      signs_staged_enable = true,
       signcolumn = true,
       numhl = false,
       linehl = false,
@@ -20,15 +28,19 @@ return {
       watch_gitdir = {
         follow_files = true,
       },
+      auto_attach = true,
       attach_to_untracked = true,
-      current_line_blame = false,
+      current_line_blame = true,
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
+        delay = 3000,
         ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
       },
       current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+      blame_formatter = nil, -- Use default
       sign_priority = 6,
       update_debounce = 100,
       status_formatter = nil, -- Use default
@@ -41,22 +53,65 @@ return {
         row = 1,
         col = 1,
       },
-      yadm = {
-        enable = false,
-      },
       on_attach = function(bufnr)
-        local keymap = vim.keymap
-        keymap.set("n", "hn", "<cmd> Gitsigns next_hunk <CR>", { desc = "Git go to next hunk" })
-        keymap.set("n", "hN", "<cmd> Gitsigns prev_hunk <CR>", { desc = "Git go to prev hunk" })
-        keymap.set("n", "ha", "<cmd> Gitsigns stage_hunk <CR>", { desc = "Git stage hunk" })
-        keymap.set("n", "hA", "<cmd> Gitsigns stage_buffer <CR>", { desc = "Git stage buffer" })
-        keymap.set("n", "hu", "<cmd> Gitsigns undo_stage_hunk <CR>", { desc = "Git Undo stage hunk" })
-        keymap.set("n", "hr", "<cmd> Gitsigns reset_hunk <CR>", { desc = "Git reset hunk" })
-        keymap.set("n", "hR", "<cmd> Gitsigns reset_buffer <CR>", { desc = "Git reset buffer" })
-        keymap.set("n", "hd", "<cmd> Gitsigns diffthis split=botright <CR>", { desc = "Git show diff" })
-        keymap.set("n", "hp", "<cmd> preview_hunk <CR>", { desc = "Git preview the hunk at the cursor position" })
-        keymap.set("v", "ha", "<cmd> Gitsigns stage_hunk <CR>", { desc = "Git stage hunk" })
-        keymap.set("v", "hr", "<cmd> Gitsigns reset_hunk <CR>", { desc = "Git reset hunk" })
+        local gitsigns = require "gitsigns"
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "hn", function()
+          if vim.wo.diff then
+            vim.cmd.normal { "hn", bang = true }
+          else
+            gitsigns.nav_hunk "next"
+          end
+        end, { desc = "Git go to next hunk" })
+
+        map("n", "hN", function()
+          if vim.wo.diff then
+            vim.cmd.normal { "hN", bang = true }
+          else
+            gitsigns.nav_hunk "prev"
+          end
+        end, { desc = "Git go to prev hunk" })
+
+        -- Actions
+        map("n", "ha", gitsigns.stage_hunk, { desc = "Git stage hunk" })
+        map("n", "hA", gitsigns.stage_buffer, { desc = "Git stage buffer" })
+        map("n", "hr", gitsigns.reset_hunk, { desc = "Git reset hunk" })
+        map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Git reset buffer" })
+        map("n", "hu", gitsigns.undo_stage_hunk, { desc = "Git undo stage hunk" })
+
+        map("v", "ha", function()
+          gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+        end)
+        map("v", "hr", function()
+          gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+        end)
+        map("v", "hu", function()
+          gitsigns.undo_stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+        end)
+
+        map("n", "hp", gitsigns.preview_hunk, { desc = "Git preview hunk at the cursor position" })
+        map("n", "hP", gitsigns.preview_hunk_inline, { desc = "Git preview the hunk at the cursor position (inline)" })
+
+        map("n", "hb", function()
+          gitsigns.blame_line { full = true }
+        end, { desc = "Git blame line" })
+
+        map("n", "hd", gitsigns.diffthis, { desc = "Git show diff" })
+        map("n", "hD", function()
+          gitsigns.diffthis "~"
+        end, { desc = "Git show diff" })
+
+        map("n", "hQ", function()
+          gitsigns.setqflist "all"
+        end, { desc = "Populate the quickfix list with all hunks" })
+        map("n", "hq", gitsigns.setqflist, { desc = "Populate the quickfix list with hunks of the current file" })
       end,
     }
   end,
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,50 +1,31 @@
 -- https://github.com/folke/trouble.nvim
 return {
   "folke/trouble.nvim",
-  tag = "v3.6.0",
   cmd = "Trouble",
   opts = {
+    auto_preview = true, -- automatically open preview when on an item
+    auto_refresh = true, -- auto refresh when open
+    focus = true, -- Focus the window when opened
     open_no_results = true, -- open the trouble window when there are no results
-    modes = {
-      diagnostics_preview = {
-        mode = "diagnostics",
-        preview = {
-          type = "split",
-          relative = "win",
-          position = "right",
-          size = 0.4,
-        },
-      },
-    },
   },
   keys = {
     {
       "xx",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+      "<cmd>Trouble diagnostics toggle<CR>",
       desc = "Trouble diagnostics",
     },
     {
       "xX",
-      "<cmd>Trouble diagnostics toggle<CR>",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
       desc = "Trouble buffer diagnostics",
     },
     {
-      "cs",
-      "<cmd>Trouble symbols toggle focus=false<CR>",
-      desc = "Trouble symbols",
-    },
-    {
-      "cl",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<CR>",
-      desc = "Trouble LSP definitions / references / ...",
-    },
-    {
-      "xL",
+      "xl",
       "<cmd>Trouble loclist toggle<CR>",
       desc = "Trouble location list",
     },
     {
-      "xQ",
+      "xq",
       "<cmd>Trouble qflist toggle<CR>",
       desc = "Trouble quickfix list",
     },
