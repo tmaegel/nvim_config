ec00c37bd9c5fa5bffcd1b70cca6932178f08967
Toni Mägel <tmaegel@posteo.de>
2026-07-24 14:45:43 +0200

    feat(treesitter): Migrate to new version

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,47 +1,37 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup {
-      ensure_installed = {
-        "bash",
-        "css",
-        "dart",
-        "dockerfile",
-        "go",
-        "gotmpl",
-        "hcl",
-        "helm",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "python",
-        "terraform",
-        "toml",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-      highlight = {
-        enable = true,
-        use_languagetree = true,
-      },
-      indent = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
+    require("nvim-treesitter").install {
+      "bash",
+      "css",
+      "dart",
+      "dockerfile",
+      "go",
+      "gotmpl",
+      "hcl",
+      "helm",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "python",
+      "terraform",
+      "toml",
+      "typescript",
+      "vim",
+      "yaml",
     }
+
+    -- Enable highlighting
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "<filetype>" },
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
   end,
 }
